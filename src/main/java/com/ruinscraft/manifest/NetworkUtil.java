5a86368b76309a939899930c3a25b1d61ce20cc9
Dexton Anderson <contact@dextonanderson.com>
2021-07-27 16:57:01 -0500

    Serialize mod manifest

--- src/main/java/com/ruinscraft/manifest/NetworkUtil.java
+++ src/main/java/com/ruinscraft/manifest/NetworkUtil.java
@@ -1,5 +1,7 @@
 package com.ruinscraft.manifest;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
@@ -12,8 +14,15 @@ public final class NetworkUtil {
     private static final Identifier CHANNEL_MODS_MANIFEST = new Identifier("manifest", "mods_manifest");
 
     public static void sendModsManifestPacket(Map<String, String> mods) {
-        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-        ClientPlayNetworking.send(CHANNEL_MODS_MANIFEST, buf);
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeInt(mods.size());
+        for (String modName : mods.keySet()) {
+            out.writeUTF(modName); // write name
+            out.writeUTF(mods.get(modName)); // write version
+        }
+        PacketByteBuf byteBuf = new PacketByteBuf(Unpooled.buffer());
+        byteBuf.writeBytes(out.toByteArray());
+        ClientPlayNetworking.send(CHANNEL_MODS_MANIFEST, byteBuf);
     }
 
 }
